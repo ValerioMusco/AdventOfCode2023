5ddbea2d2b5ceefc9bc72c037fb7e41b5a4d220a
Valerio PC Techni <byleadx@hotmail.fr>
2023-12-07 16:21:12 +0100

    Day 4 - Part 1 done

---
+++ AdventOfCode2023-Day4/Games.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023_Day4 {
+    public class Games {
+
+        public Games( string input ) {
+
+            string gameInfo = input.Split(": ")[0];
+            gameInfo = gameInfo.Split( "d" )[1];
+            GameId = int.Parse(gameInfo.Trim());
+
+            string game = input.Split(": ")[1];
+            string Winning = game.Split(" | ")[0];
+            string Owned = game.Split(" | ")[1];
+
+            string[] Winnings = Winning.Split(" ");
+            string[] Owneds = Owned.Split(" ");
+
+            WinningNumbers = new();
+            OwnedNumbers = new();
+
+            foreach(string win in  Winnings )
+                if(win != String.Empty)
+                    WinningNumbers.Add(int.Parse(win.Trim()));
+            
+            foreach(string own in Owneds)
+                if(own != String.Empty)
+                    OwnedNumbers.Add(int.Parse(own.Trim()));
+
+            // Part 1
+            // GamePoints = (int)Math.Pow(2, WinningNumbers.Intersect(OwnedNumbers).Count() - 1);
+
+            //Part 2
+            GamePoints = WinningNumbers.Intersect( OwnedNumbers ).Count();
+        }
+
+        public int GameId { get; set; }
+        public List<int> WinningNumbers { get; set; }
+        public List<int> OwnedNumbers { get; set; }
+        public int GamePoints { get; set; }
+    }
+}
---
+++ AdventOfCode2023-Day4/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2023_Day4;
+using System.Net.WebSockets;
+
+string path = @"C:\Users\Ugo est nul\Desktop\Prog\C#\AdventOfCode2023\AdventOfCode2023-Day4\TestValue.txt";
+
+List<string> inputs = GetFileContent(path);
+List<Games> games = new();
+int sum = 0;
+
+foreach (string  input in inputs)
+    games.Add(new Games(input));
+
+foreach( Games game in games )
+    Console.WriteLine(game.GamePoints);
+
+Console.WriteLine(sum);
+
+#region Methods
+static List<string> GetFileContent( string path ) {
+
+    List<string>inputs = new();
+
+    if( File.Exists( path ) )
+        using( StreamReader sr = new( path ) ) {
+            string line;
+            while( ( line = sr.ReadLine() ) != null )
+                inputs.Add( line.Trim() );
+        }
+    return inputs;
+}
+
+#endregion
