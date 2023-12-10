a56e5a3bddb128a5943744cd131e7e0dcde56893
Valerio PC Fixe <byleadx@hotmail.fr>
2023-12-10 22:28:14 +0100

    Day 5 part 1 done

--- AdventOfCode2023-Day5/Maps.cs
+++ AdventOfCode2023-Day5/Maps.cs
@@ -8,59 +8,33 @@ using System.Threading.Tasks;
 namespace AdventOfCode2023_Day5 {
     public class Maps {
 
-        public Maps( string map, long max ) {
+        public Maps( string map ) {
 
-            Descriptor = map.Split( ":\n" )[0];
             string mapsInfo = map.Split( ":\n" )[1];
             string[] mapsValues = mapsInfo.Split("\n");
 
-            List<long> destinationRangeStart = new();
-            List<long> sourceRangeStart = new();
-            List<long> rangeLength = new();
+            DestinationRangeStart = new();
+            SourceRangeStart = new();
+            RangeLength = new();
 
             foreach( string mapValue in mapsValues ) {
 
                 string[] values = mapValue.Split( " " );
-                destinationRangeStart.Add( long.Parse( values[0] ) );
-                sourceRangeStart.Add( long.Parse( values[1] ) );
-                rangeLength.Add( long.Parse( values[2] ) );
+                DestinationRangeStart.Add( long.Parse( values[0] ) );
+                SourceRangeStart.Add( long.Parse( values[1] ) );
+                RangeLength.Add( long.Parse( values[2] ) );
             }
-
-            DestinationRangeStart = destinationRangeStart.ToArray();
-            SourceRangeStart = sourceRangeStart.ToArray();
-            RangeLength = rangeLength.ToArray();
-
-            GenerateMap(max);
         }
 
-        public string Descriptor { get; set; }
-        public long[] DestinationRangeStart { get; set; }
-        public long[] SourceRangeStart { get; set; }
-        public long[] RangeLength { get; set; }
+        public List<long> DestinationRangeStart { get; set; }
+        public List<long> SourceRangeStart { get; set; }
+        public List<long> RangeLength { get; set; }
 
-        public long[,] Map { get; set; }
-
-        private void GenerateMap(long max) {
-
-            Map = new long[max > SourceRangeStart.Max() + RangeLength.Max() ? max : SourceRangeStart.Max() + RangeLength.Max(), 2];
-
-            for( long i = 0; i < (Map.LongLength)/2; i++ ) {
-
-                Map[i, 0] = i;
-                Map[i, 1] = i;
-            }
-            
-
-            for(long i = 0; i < RangeLength.Length; i++ )
-                for(long j = 0; j < RangeLength[i]; j++ )
-                    Map[SourceRangeStart[i] + j, 1] = DestinationRangeStart[i] + j;
-        }
 
         public override string ToString() {
             
             StringBuilder sb = new();
 
-            sb.AppendLine( $"Descriptor : {Descriptor}");
             sb.Append( "Destination : " );
             foreach( long destinatioinRangeStart in DestinationRangeStart )
                 sb.Append( destinatioinRangeStart + " " );
--- AdventOfCode2023-Day5/Program.cs
+++ AdventOfCode2023-Day5/Program.cs
@@ -1,20 +1,48 @@
 ﻿using AdventOfCode2023_Day5;
 
-string path = @"E:\Progra\C#\AdventOfCode2023\AdventOfCode2023-Day5\Sample.txt";
+string path = @"E:\Progra\C#\AdventOfCode2023\AdventOfCode2023-Day5\TestValue.txt";
 
 string input = GetFileContent(path);
 
 List<long> seeds = ParseSeedsFromInput(input);
-List<Maps> maps = ParseMapsFromInput( input, seeds.Max() );
+List<Maps> maps = ParseMapsFromInput( input );
 
-List<long> location = new();
+List<long>  locations = new();
+long location;
 
-foreach(long seed in seeds ) {
+#region Part 1
 
-    location.Add(GetLocation(maps, seed));
+//foreach( long seed in seeds ) {
+
+//    location = seed;
+//    long tempValueToSubstract = seed;
+
+//    foreach(Maps map in maps ) 
+//        for( int i = 0; i < map.RangeLength.Count; i++ ) 
+//            if( location > map.SourceRangeStart[i] && location <= map.SourceRangeStart[i] + map.RangeLength[i] - 1 ) {
+
+//                tempValueToSubstract = map.SourceRangeStart[i] + map.RangeLength[i] - 1 - location;
+//                location = map.DestinationRangeStart[i] + map.RangeLength[i] - 1;
+//                location -= tempValueToSubstract;
+//                tempValueToSubstract = location;
+//                break;
+//            }
+
+//    locations.Add( location );
+//}
+
+#endregion
+
+for(int i = 0; i < seeds.Count / 2; i++ ) {
+
+    for(int j = 0; j < seeds[i+1 * 2]; j++ ) {
+        Console.Write( seeds[i * 2] + j  + " ");
+    }
+
+    Console.WriteLine();
 }
 
-Console.WriteLine(location.Min());
+//Console.WriteLine(locations.Min());
 
 #region Methods
 static string GetFileContent( string path ) {
@@ -41,24 +69,15 @@ List<long> ParseSeedsFromInput( string input ) {
     return seeds;
 }
 
-List<Maps> ParseMapsFromInput( string input, long max ) {
+List<Maps> ParseMapsFromInput( string input ) {
 
     List<Maps> maps = new();
     string[] mapsToParse = input[input.Split("\n")[0].Length..].Trim().Split("\n\n");
 
     foreach( string mapToParse in mapsToParse )
-        maps.Add( new( mapToParse, max ) );
+        maps.Add( new( mapToParse ) );
 
     return maps;
-}
-
-long GetLocation(List<Maps> maps, long source) {
-
-    long location = source;
-    foreach(Maps map in maps )
-        location = map.Map[location, 1];
-
-    return location;
 }
 
 #endregion
